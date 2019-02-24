0c16d71a8b603217d0c8b63fc6e4d1b42c47f449
pkepp11-7 <pkpitcher@hotmail.com>
2019-02-24 02:26:24 -0800

    Commit to sync project

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -61,14 +61,19 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if(segue.identifier == "toStoriesList") {
+            let storiesListVC = segue.destination as! ProfileStoryTableController
+            storiesListVC.keyTitlePairs = myStats.userPosts
+        }
+        
     }
-    */
+
 
 }
